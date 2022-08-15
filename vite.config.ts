8dc5d978ec4407e0de8abaf4be58821dfcc39866
Shahriar Shojib <shahriar_shojib@hotmail.com>
2022-08-16 00:41:44 +0600

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { crx } from '@crxjs/vite-plugin';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import buildTimeImport from 'postcss-import';
+import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
+import manifest from './manifest.json';
+
+// https://vitejs.dev/config/
+export default defineConfig((config) => {
+	return {
+		plugins: [
+			svelte(),
+			crx({
+				manifest,
+			}),
+		],
+		css: {
+			postcss: {
+				plugins: [tailwindcss, buildTimeImport, ...(config.command === 'build' ? ['cssnano'] : [])],
+			},
+			devSourcemap: true,
+		},
+	};
+});
